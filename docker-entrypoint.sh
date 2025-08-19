d526a663188c86838a420cd539a08a21ae83782a
pputman-clabs <patrick.putman@clabs.co>
2025-08-19 09:42:02 -0500

    testing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Change ownership of the mounted volume to the fluent user (by name).
-chown -R fluent:fluent /fluentd/log
+# Change ownership of the mounted volume to the fluent user (UID 1000).
+chown -R 1000:1000 /fluentd/log
 
 # Execute the main command from the Dockerfile
 exec "$@"
